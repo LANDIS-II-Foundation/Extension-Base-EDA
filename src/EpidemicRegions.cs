8931e288bfa03c393c08b8016487542dee0350f7
f-tonini <ftonini84@gmail.com>
2016-01-20 15:05:11 -0500

    first commit - created new class for input epidem locations map

---
+++ src/EpidemicRegions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using Landis.SpatialModeling;
+
+namespace Landis.Extension.BaseEDA
+{
+    public class EpidemicRegions
+    {
+
+        //---------------------------------------------------------------------
+        public static void ReadMap(string path)
+        {
+            IInputRaster<BytePixel> map;
+
+            try
+            {
+                map = PlugIn.ModelCore.OpenRaster<BytePixel>(path);
+            }
+            catch (FileNotFoundException)
+            {
+                string mesg = string.Format("Error: The file {0} does not exist", path);
+                throw new System.ApplicationException(mesg);
+            }
+
+            if (map.Dimensions != PlugIn.ModelCore.Landscape.Dimensions)
+            {
+                string mesg = string.Format("Error: The input map {0} does not have the same dimension (row, column) as the ecoregions map", path);
+                throw new System.ApplicationException(mesg);
+            }
+
+            using (map)
+            {
+                BytePixel pixel = map.BufferPixel;
+                foreach (Site site in PlugIn.ModelCore.Landscape.AllSites)
+                {
+                    map.ReadBufferPixel();
+                    byte mapCode = pixel.MapCode.Value;
+                    if (mapCode > 1)
+                    {
+                        string mesg = string.Format("Error: The input map {0} must have 0-1 values, where 0 = susceptible, 1 = infected", path);
+                        throw new System.ApplicationException(mesg);
+                    }
+
+                    if (site.IsActive)
+                    {
+                        SiteVars.InfStatus[site] = mapCode;
+                        if (mapCode == 0)
+                        {
+                            SiteVars.PSusceptible[site] = 1;
+                            SiteVars.PInfected[site] = mapCode;
+                        }                            
+                        else
+                        {
+                            SiteVars.PSusceptible[site] = 0;
+                            SiteVars.PInfected[site] = mapCode;
+                        }                            
+                    }
+                }
+            }
+        }
+
+    }
+}
