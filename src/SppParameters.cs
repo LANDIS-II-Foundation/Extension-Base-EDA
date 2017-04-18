c20040ede61b51b9a26c6214b9bc6269c95620e7
ChrisJones687 <cjones1688@gmail.com>
2017-04-18 10:19:23 -0400

    Pushing final model back to master

--- src/SppParameters.cs
+++ src/SppParameters.cs
@@ -1,6 +1,6 @@
 //  Copyright 2016 North Carolina State University, Center for Geospatial Analytics & 
 //  Forest Service Northern Research Station, Institute for Applied Ecosystem Studies
-//  Authors:  Francesco Tonini, Brian R. Miranda
+//  Authors:  Francesco Tonini, Brian R. Miranda, Chris Jones
 
 using Edu.Wisc.Forest.Flel.Util;
 
